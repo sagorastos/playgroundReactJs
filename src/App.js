df0867f0e1a8a70744de884cf0d940477715f168
Spyros Agorastos <spyrosagorastos96@gmail.com>
2023-08-10 23:15:40 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+const Hello = (props) => {
+  return (
+    <div>
 
+      <p>Hello {props.name}</p>
+    </div>
+  )
+} 
 const App = () => {
   return (
     <div>
