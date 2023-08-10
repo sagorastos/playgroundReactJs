1dbdefb61622915d325a54ad70ee7fbb18ab1f30
Spyros Agorastos <spyrosagorastos96@gmail.com>
2023-08-10 23:09:41 +0300

    create branch test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-const Hello = (props) => {
-  return (
-    <div>
-
-      <p>Hello {props.name}</p>
-    </div>
-  )
-}
 
 const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
 
-      <Hello  name='George'/>
+     
     </div>
   )
 }
